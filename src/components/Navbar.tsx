0820a24a1890ea1a11ca6ae60f8f696049e4a337
Christian <ch.molnar@gmail.com>
2026-04-24 08:40:48 +0200

    Unify landing page polish: hero refinements, pricing/i18n fixes, founder section restore, and updated iOS popup copy

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 const languages: Language[] = ["de", "en", "es", "fr", "ru"];
 const GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.christian.moonli&hl=en";
-const IOS_SOON_TEXT = "Wir arbeiten mit Hochdruck an der iPhone-Version. Sie ist bald fuer dich verfuegbar.";
+const IOS_SOON_TEXT = "Wir arbeiten mit Hochdruck an der iPhone-Version. Sie ist bald für dich verfügbar.";
 
 const Navbar = () => {
   const { language, setLanguage, t } = useLanguage();
